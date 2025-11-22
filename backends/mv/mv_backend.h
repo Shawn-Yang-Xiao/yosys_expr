db33a47d24cf60cb904333ad2fbb9433d1d31f19
Shawn-Yang-Xiao <2064872641@qq.com>
2025-11-22 22:58:11 +0800

    write mv_backend lib

--- backends/mv/mv_backend.h
+++ backends/mv/mv_backend.h
@@ -3,7 +3,7 @@
 
 
 #include <memory> // support shared_ptr
-#include <cstring>
+#include <string>
 #include <vector>
 #include <set>
 
@@ -13,20 +13,24 @@
 
 YOSYS_NAMESPACE_BEGIN
 
-struct MV_BACKEND::HwVar {
+namespace MV_BACKEND {
+
+struct HwVar {
     enum VarKind { WIRE, CONST } kind;
     std::string wire_name;      // if kind == WIRE
     bool const_val = false;     // if kind == CONST
 
-    explicit HwVar(std::string &w) : kind(WIRE), wire_name(w) { };
+    HwVar() : kind(CONST), const_val(false) { }
+
+    explicit HwVar(const std::string &w) : kind(WIRE), wire_name(w) { };
     explicit HwVar(bool b) : kind(CONST), const_val(b) { };
 
-    static HwVar wire(const std::string &name) { return HwVar(name); }
-    static HwVar constant(bool var) { return HwVar(val); }
+    static HwVar make_wire(const std::string &name) { return HwVar(name); }
+    static HwVar make_constant(bool val) { return HwVar(val); }
 };
 
 
-enum class MV_BACKEND::Operator {
+enum class Operator {
     ADD,
     MUL,
     NEG,
@@ -34,7 +38,7 @@ enum class MV_BACKEND::Operator {
 };
 
 
-struct MV_BACKEND::HwExpr {
+struct HwExpr {
     enum class ExprType {
         VAR,
         OP1,
@@ -46,44 +50,55 @@ struct MV_BACKEND::HwExpr {
     HwVar var;                  // valid when type == VAR 
 
     // Is there problem with deep copy or shallow copy? 
+    // Is default construction method needed?
+    HwExpr() : type(ExprType::VAR), op(Operator{}), var() { }
 
-    static HwExpr var(const HwVar& v) {
-        HwExpr ret;
-        ret.type = VAR;
-        ret.var = v;
-        return ret;
+    explicit HwExpr(ExprType et, std::vector<HwExpr*> exprVec, Operator op, HwVar hv) : type(et), args(exprVec), op(op), var(hv) { };
+
+    static HwExpr make_var(const HwVar& v) {
+        return HwExpr{
+            ExprType::VAR,
+            {},
+            Operator {},
+            v
+        };
     }
-    static HwExpr unary(Operator op, HwExpr* a) {
-        HwExpr ret;
-        ret.type = OP1;
-        ret.op = op;
-        ret.args.push_back(a);
-        return ret;
+
+    static HwExpr make_unary(Operator op, HwExpr* a) {
+        return HwExpr {
+            ExprType::OP1,
+            {a},
+            op,
+            HwVar{ }
+        };
     }
-    static HwExpr binary(Operator op, HwExpr* a, HwExpr* b) {
-        HwExpr ret;
-        ret.type = OP2;
-        ret.op = op;
-        ret.args.push_back(a);
-        ret.args.push_back(b);
-        return ret;
+
+    static HwExpr make_binary(Operator op, HwExpr* a, HwExpr* b) {
+        return HwExpr {
+            ExprType::OP2,
+            {a, b},
+            op,
+            HwVar{ }
+        };
     }
-    static HwExpr nary(Operator op, std::vector<HwExpr*> operands) {
-        HwExpr ret;
-        ret.type = OPN;
-        ret.op = op;
-        ret.args = operands;
-        return ret;
+
+    static HwExpr make_nary(Operator op, std::vector<HwExpr*> operands) {
+        return HwExpr {
+            ExprType::OPN,
+            std::move(operands),
+            op,
+            HwVar{ }
+        };
     }
 };
 
 
-struct MV_BACKEND::HwInstruction {
+struct HwInstruction {
     enum InstrKind {
         IK_subst,   // :=
         IK_glitch,  // =![]
-        Leak,       // leak instruction
-        MCall,      // call a module
+        IK_leak,       // leak instruction
+        IK_mcall,      // call a module
     } kind;
     // in maskVerif, parsetree.ml, definition
     // type instr_kind = 
@@ -97,167 +112,78 @@ struct MV_BACKEND::HwInstruction {
     HwExpr rhs;
     // for leak
     std::string leak_name;
-    std::set<Expr> leak_exprs;
+    std::vector<HwExpr> leak_exprs; // This will likely be replaced by value expressions.
+    // manually remove duplicates before assignment
     // for mcall
     std::string callee_module;
     std::vector< std::pair<std::string, std::HwExpr> > port_bindings;
 
+    HwInstruction() : kind(InstrKind::IK_subst), lhs(), rhs() {}
+
+    explicit HwInstruction(InstrKind ik, HwVar lhv, HwExpr rhe, std::string lkn, std::vector<HwExpr> lkes, std:string cem, std::vector< std::pair<std::string, std::HwExpr> > pbs) : kind(ik), lhs(lhv), rhs(rhe), leak_name(lkn), leak_exprs(lkes) callee_module(cem), port_bindings(pbs) { };
+
     static HwInstruction subst(const HwVar& lhs, const HwExpr& rhs){
-        HwInstruction inst;
-        inst.kind = IK_subst;
-        inst.lhs = lhs;
-        assert(lhs.kind == HwVar::WIRE);
-        inst.rhs = rhs;
-        return inst;
+        assert(lhs.kind == VarKind::WIRE);
+        return HwInstruction {
+            InstrKind::IK_subst,
+            lhs,
+            rhs,
+            "",
+            {},
+            "",
+            {}
+        };
     }
 
     static HwInstruction glitch(const HwVar& lhs, const HwExpr& rhs) {
-        HwInstruction inst;
-        inst.kind = IK_glitch;
-        inst.lhs = lhs;
-        assert(lhs.kind == HwVar::WIRE);
-        inst.rhs = rhs;
-        return inst;
+        assert(lhs.kind == VarKind::WIRE);
+        return HwInstruction {
+            InstrKind::IK_glitch,
+            lhs,
+            rhs,
+            "",
+            {},
+            "",
+            {}
+        };
     }
 
-    static HwInstruction leak(const std::string& str, const std::set<Expr> expr_set) {
-        HwInstruction inst;
-        inst.kind = IK_leak;
-        inst.leak_name = str;
-        inst.leak_exprs = expr_set;
-        return inst;
+    static HwInstruction leak(const std::string& str, const std::vector<Expr> expr_vec) {
+        return HwInstruction{
+            InstrKind::IK_leak,
+            {},
+            {},
+            str,
+            std::move(expr_vec), // this should be a set, manually remove duplicates beforehead
+            "",
+            {}
+        };
     }
 
-    static HwInstruction mcall(cosnt std::string& mod_name, std::vector< std::pair<std::string, std::HwExpr> > bindings) {
-        HwInstruction inst;
-        inst.kind = IK_mcall;
-        inst.callee_module = mod_name;
-        inst.port_bindings = bindings;
-        return inst;
+    static HwInstruction mcall(cosnt std::string& mod_name, std::vector< std::pair<std::string, HwExpr> > bindings) {
+        return HwInstruction {
+            InstrKind::IK_mcall,
+            {},
+            {},
+            "",
+            {},
+            mod_name,
+            std::move(bindings)
+        };
     }
 };
 
 
 
 
-struct MV_BACKEND::HwCellDef {
+struct HwCellDef {
     std::string cell_type;  // cell type name, like $_XNOR_
     std::vector<std::string> inputs;
     std::string output;
     std::vector<HwInstruction> instructions; 
 };
 
-
-// define expr
-struct MV_BACKEND::Var {
-    std::string name; 
-    Var(const std::string &n) : name(n) { }
-
-    bool operattor==(const Var &b) const { return name == b.name; }
-    Hasher hash_into(Hasher h) const { return mkhash(h,nmae.c_str()) };
-};
-// FIXME: there is no const, what if a cell input is const
-// how does maskVerif deal with const in Ilang file?
-
-
-enum class OpKind {
-    Add,
-    Mul,
-    Neg,
-    Other
-};
-
-struct Operator {
-    bool is_bij;
-    OpKind kind;
-    Operator( bool bij, OpKind k) 
-        : is_bij(bij), kind(k) { }
-};
-
-struct Expr;
-
-// using ExprPtr = std::shared_ptr<Expr>;  // support shared subexpression
-// QUESTION: Is this necessary?
-
-
-struct Expr {
-    enum class Op {
-        VAR,
-        CONST,
-        OP1,
-        OP2,
-        OPN
-    };
-    Operator op;
-    Var* var; // when the expr is a var
-    bool const_val = false; // when expr is a const, true or false
-    std::vector<Expr*> args;
-};
-
-/*
-struct Expr {
-    int e_id;
-    enum class NodeType {
-        ETop,
-        Ernd,
-        Eshare,
-        Epub,
-        Eop1,
-        Eop2,
-        Eop,
-        Econst
-    }type; 
-    Var* var = nullptr;         // for Ernd, Epub, Eshare
-    int share_index = 0;        // for Eshare
-    Var* share_param = nullptr; // for Eshare
-    Operator* op = nullptr;     // for Eop1, Eop2, Eop
-    ExprPtr arg1, arg2;         // for Eop1, Eop2
-    std::vector<ExprPtr> args;  // for Eop
-    bool const_val = false;     // for Econst
-};
-*/
-
-
-struct Instruction {
-    enum class InstructionKind {
-        IK_subst,   // := 
-        IK_glitch,  // =![ ] 
-        Leak,       // leak instruction 
-        MCall       // call a module
-    } kind;
-    /*
-    in maskVerif, parsetree.ml, definition
-    type instr_kind =
-        | IK_subst (:=)
-        | IK_hide (= [ ])
-        | IK_sub (=)
-        | IK_glitch (=![ ])
-        | IK_noleak (<-)
-    */
-    Var lhs;       // left-hand side variable, used in IK_subst and IK_glitch
-    Expr* rhs;    // right-hand side expression
-};
-
-struct ModuleExpr {
-    RTLIL::IdString name;
-    std::vector<RTLIL::IdString> input_ports;
-    RTLIL::IdString output_port;
-    std::vector<Instruction> instructions;
-};
-
-struct DesignExpr {
-    dict<RTLIL::IdString, ModuleExpr*> modules_;
-    RTLIL::IdString top_module;
-};
-
-// For sorting cells in simcell lib modules. 
-// Record predecessor and successors of a wire in cell
-struct WireConn {
-    RTLIL::IdString wire_name;
-    RTLIL::IdString pred;
-    std::vector<RTLIL::IdString> succs;
-};
-
+} // namespace MV_BACKEND
 
 YOSYS_NAMESPACE_END
 
